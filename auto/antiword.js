31e096490f123f3b4dcc158133f47f5cf4f8dfb9
yaydy <107906477+yaydy@users.noreply.github.com>
2022-07-16 05:51:40 +0000

    added auto + antiword (may or may not work idk )

---
+++ auto/antiword.js
@@ -0,0 +1,10 @@
+client.on("MessageCreate", message =>{
+const wordz = ["sus", "amongus", "bald", "bozo"]; 
+if (wordz.some(word => message.content.toLowerCase().includes(word.toLowerCase()))) { 
+  message.reply("bad word bad");
+  delay(2500);
+  message.delete();
+  console.log("blocked word");
+  }
+}
+console.log("blocking " + wordz);
