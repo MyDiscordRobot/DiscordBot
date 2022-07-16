e93ab0d5a569a1cf330494b6fed680b07f79a232
yaydy <107906477+yaydy@users.noreply.github.com>
2022-07-16 06:13:26 +0000

    auto removed

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const Database = require("@replit/database");
 const fs = require("fs");
 const client = new Client({
   intents:
-    ["GUILDS", 
+    [
+     "GUILDS", 
      "GUILD_MESSAGES",
      "GUILD_MEMBERS",
      "GUILD_MESSAGE_REACTIONS",
      "GUILD_WEBHOOKS",
      "GUILD_VOICE_STATES",
      "GUILD_INVITES",
-     "GUILD_BANS"]
+     "GUILD_BANS"
+    ]
 })
 
 const config = require("./config.json");
@@ -18,17 +20,16 @@ client.config = config;
 client.commands = new Collection();
 
 const events = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-for (const file of events) {
+for (const file of events){
   const eventName = file.split(".")[0];
   const event = require(`./events/${file}`);
   client.on(eventName, event.bind(null, client));
 }
 
 const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-for (const file of commands) {
+for (const file of commands){
   const commandName = file.split(".")[0];
   const command = require(`./commands/${file}`);
-
   console.log(`Attempting to load command ${commandName}`);
   client.commands.set(commandName, command);
 }
