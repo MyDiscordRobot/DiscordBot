8c055b5a663831c579de153daa6d95d3131e721a
yaydy <107906477+yaydy@users.noreply.github.com>
2022-07-18 06:10:19 +0000

    more code cleanup

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,20 +2,19 @@ const talkedRecently = new Set();
 
 module.exports = (client, message) => {
   if (message.author.bot) return;
-  const prefix = process.env.prefix;
-  if (message.content.indexOf(prefix) !== 0) return;
+  if (message.content.indexOf(process.env.prefix) !== 0) return;
   if (talkedRecently.has(message.author.id)) return message.reply("Don't spam commands!");
   talkedRecently.add(message.author.id);
   setTimeout(() => {
   talkedRecently.delete(message.author.id);
   }, 2000);
+  console.log(process.env.prefix)
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   const cmd = client.commands.get(command);
-
 if (!cmd){
     message.reply("That command does not exist. Use $help for a list of existing commands.");
-  }else{  
+  } else {  
   cmd.run(client, message, args);
   }
 };
