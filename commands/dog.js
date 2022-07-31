4beb1a7a9fd29eee91783a493040c74397086277
yaydy <107906477+yaydy@users.noreply.github.com>
2022-07-31 00:12:45 +0000

    dog command

--- commands/dog.js
+++ commands/dog.js
@@ -1,12 +1,15 @@
-const randomPuppy = require('random-puppy');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
 exports.run = async (client, message, args) => {
-randomPuppy()
-console.log(randomPuppy.url)
- const embed = new MessageEmbed()
-    .setTitle("Dog!")
-    .setImage(randomPuppy.url)
-await message.reply({ embeds: [embed] }) && message.react("✅");
+const url = await fetch("https://www.reddit.com/r/dog/random/.json?include_over_18=off");
+const random = await url.json();
+
+const embed = new MessageEmbed()
+  .setTitle("Dog!")
+  .setImage(random[0].data.children[0].data.url)
+
+await message.channel.send({ embeds: [embed] });
 }
 
 exports.name = "dog";
