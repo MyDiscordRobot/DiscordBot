3da68b065c6e1cb17e94165278064a35ce34c5b0
yaydy <107906477+yaydy@users.noreply.github.com>
2022-08-01 23:12:40 +0000

    bling

--- commands/random/cat.js
+++ commands/random/cat.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 
 exports.run = async (client, message, args) => {
-  const url = await fetch("https://www.reddit.com/r/cat/random/.json?include_over_18=off");
+  const url = await fetch("https://www.reddit.com/r/CatPics/random/.json?include_over_18=off");
   const random = await url.json();
 
   const embed = new MessageEmbed()
