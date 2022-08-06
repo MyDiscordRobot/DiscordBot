3279667ec0058dca0f034e103787ffdd6207762d
yaydy <107906477+yaydy@users.noreply.github.com>
2022-08-06 23:59:09 +0000

    crash command cleaned

--- commands/owner/crash.js
+++ commands/owner/crash.js
@@ -1,5 +1,7 @@
-exports.run = (client, message) => {
+exports.run = (message) => {
   if(message.author.id == process.env.SuperUserID){
+    message.react("🏠");
+    message.react("🚗");
     console.log("Process was killed by owner.");
     process.exit(0);
   } else {
