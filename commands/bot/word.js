c7ccd00fc9271755ec5dc6bc84bc8c17d5251905
Yaydy <107906477+yaydy@users.noreply.github.com>
2022-09-30 17:17:57 +0000

    starting fix

--- commands/bot/word.js
+++ commands/bot/word.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
 const fetch = require('node-fetch');
 if(!args) return message.reply("You need to put a word!");
 const word = args;
-fetch('https://words.bighugelabs.com/api/2/3c45af9010a4c726148ed6fbe83dd590/${word}/json')
+fetch('https://words.bighugelabs.com/api/2/${api}/${word}/json')
   .then(response => response.json())
   .then(data => {
     console.log(data)
